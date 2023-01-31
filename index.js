f07f1573c056eb7eff9c913c4fc9265f31f18d12
Ria Dutta <riaduttacoder@gmail.com>
2023-01-31 16:16:24 +0530

    second commit

--- index.js
+++ index.js
@@ -3,27 +3,28 @@ const CsvReadableStream = require('csv-reader');
 let inputStream = Fs.createReadStream('info.csv', 'utf8');
 let count = 0;
 let json = {};
+let attr = [];
+let obj = {};
 
 inputStream
     .pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
     .on('data', function (row) {
-        let attr = [];
         if (count == 0) {
             attr = row;
-            json['persons'] = []
+            json['persons'] = [];
         }
         else {
-            let obj1 = {};
+            obj = {};
             for (let key in row) {
                 if (key == 0) {
-                    obj1[attr[key]] = row[key];
-                    obj1['details'] = {};
+                    obj[attr[key]] = row[key];
+                    obj['details'] = {};
                 }
                 else {
-                    obj1['details'][attr[key]] = row[key];
+                    obj['details'][attr[key]] = row[key];
                 }
             }
-            json['persons'].push(obj1);
+            json['persons'].push(obj);
         }
         count++;
     });
